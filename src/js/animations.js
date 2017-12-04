9b95e0abb11e0f97f64d85b4c66ad45de5fd801c
Pritak Patel <pritak.patel@gmail.com>
2017-12-04 15:46:00 -0800

    Footer modified, responsive design fixes

--- src/js/animations.js
+++ src/js/animations.js
@@ -12,6 +12,10 @@ $(document).ready(function () {
         }
     });
 
+    setInterval(function () {
+        $('#heart-footer').animateCss("pulse");
+    }, 2000);
+
     var animationOptions = ["bounce", "flash", "pulse", "rubberBand",
         "shake", "headShake", "swing", "tada",
         "wobble", "jello", "bounceIn", "bounceInDown",
