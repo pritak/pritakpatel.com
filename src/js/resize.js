a78b3eab22567ac26f4bddd7d34a78da1aee0555
Pritak Patel <pritak.patel@gmail.com>
2017-12-04 02:10:34 -0800

    Redesigned left panel, added resize options, redone icons

--- src/js/resize.js
+++ src/js/resize.js
@@ -1,8 +1,26 @@
 var $window = $(window);
 
 $(document).ready(function () {
+
+    $('#click-toggle').click(function(){
+        var toggle = $('#left-column').hasClass('is-half');
+        $(this).toggleClass('flip');
+        $('#left-column').toggleClass('is-half', !toggle);
+        $('#right-column').toggleClass('is-half', !toggle);
+        $('#filler-column').toggleClass('is-half', !toggle);
+        $('#left-column').toggleClass('is-one-third', toggle);
+        $('#right-column').toggleClass('is-two-thirds', toggle);
+        $('#filler-column').toggleClass('is-one-third', toggle);
+        return;
+    });
+
     if ($window.width() < 768) {
         $('#title-hero').removeClass('is-fullheight');
+        return;
+    }
+    if ($window.width() < 1000) {
+        $('#navigation').removeClass('is-size-6');
+        $('#navigation').addClass('is-size-7');
         return;
     }
 });
@@ -17,7 +35,7 @@ $(document).ready(function () {
         if ($window.width() > 768) {
             $('#title-hero').addClass('is-fullheight');
             return;
-        }
+        } 
     }
     $window
         .resize(resize)
--- src/js/title-design.js
+++ src/js/title-design.js
@@ -13,15 +13,31 @@ $(document).ready(function () {
   //   animateFill: true
   // });
 
-  var mouseX, mouseY;
-  var ww = $(window).width();
-  var wh = $(window).height();
-  var traX, traY;
-  $(document).mousemove(function (e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-    traX = ((4 * mouseX) / 570) + 40;
-    traY = ((4 * mouseY) / 570) + 50;
-    $(".title").css({ "background-position": traX + "%" + traY + "%" });
-  });
+  // var mouseX, mouseY;
+  // var ww = $(window).width();
+  // var wh = $(window).height();
+  // var traX, traY;
+  // $(document).mousemove(function (e) {
+  //   mouseX = e.pageX;
+  //   mouseY = e.pageY;
+  //   traX = ((4 * mouseX) / 570) + 40;
+  //   traY = ((4 * mouseY) / 570) + 50;
+  //   $(".title").css({ "background-position": traX + "%" + traY + "%" });
+  // });
+
+  var subtitles2 = ['Design', 'Photography', 'Art', 'Travel', 'Hiking', 'Badminton', 'Music', 'Film'];
+  var subtitles1 = ['Software Engineer', 'Electrical Engineer', 'Computer Scientist', 'Tech Consultant'];
+
+  setInterval(function() {
+    var rand1 = Math.floor(Math.random() * 8);
+    var rand2 = Math.floor(Math.random() * 4);
+    $("#subtitle-change-2").fadeOut(800, function () {
+      $("#subtitle-change-2").text(subtitles2[rand1]).fadeIn(800);
+    });
+    $("#subtitle-change-1").fadeOut(800, function () {
+      $(this).text(subtitles1[rand2]).fadeIn(800);
+    });
+  }, 5000);
+
+
 });
